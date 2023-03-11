7d5a285be99b5ca24456535b02a451540d2ca281
Denis Ryzhankov <denryzhankov@gmail.com>
2023-03-11 21:36:25 +0300

    edit login.html

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, url_for
+from flask_login import LoginManager, login_user
 
 from webapp.forms import LoginForm
-from webapp.model import News, db
+from webapp.model import News, User, db
 
 
 def create_app():
@@ -21,5 +22,25 @@ def create_app():
         title = "Авторизация"
         login_form = LoginForm()
         return render_template("login.html", page_title=title, form=login_form)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = "login"
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
+
+    @app.route("/process-login", methods=["POST"])
+    def process_login():
+        form = LoginForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(username=form.username.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                flash("Вы вошли на сайт")
+                return redirect(url_for("index"))
+        flash("Неправильное имя пользователя или пароль")
+        return redirect(url_for("login"))
 
     return app
