5bb7796e11d6c98184f6d0679e380c24403855c1
Denis Ryzhankov <denryzhankov@gmail.com>
2023-03-11 20:17:12 +0300

    add login

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,16 +1,25 @@
 from flask import Flask, render_template
-from webapp.python_org_news import get_python_news
-from webapp.model import db, News
+
+from webapp.forms import LoginForm
+from webapp.model import News, db
+
 
 def create_app():
     app = Flask(__name__)
 
-    app.config.from_pyfile('config.py')
+    app.config.from_pyfile("config.py")
     db.init_app(app)
 
-    @app.route('/')
+    @app.route("/")
     def index():
         title = "Новости Python"
         news_list = News.query.order_by(News.published.desc()).all()
-        return render_template('index.html', page_title=title, news_list=news_list)
+        return render_template("index.html", page_title=title, news_list=news_list)
+
+    @app.route("/login")
+    def login():
+        title = "Авторизация"
+        login_form = LoginForm()
+        return render_template("login.html", page_title=title, form=login_form)
+
     return app
--- webapp/model.py
+++ webapp/model.py
@@ -1,9 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy.orm.decl_api import Model
 
 db = SQLAlchemy()
+base: Model = db.Model
 
-class News(db.Model):
+
+class News(base):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     url = db.Column(db.String, unique=True, nullable=False)
@@ -11,4 +13,8 @@ class News(db.Model):
     text = db.Column(db.Text, nullable=True)
 
     def __repr__(self):
-        return '<News {} {}>'.format(self.title, self.url)
+        return "<News {} {}>".format(self.title, self.url)
+
+
+if __name__ == "__main__":
+    new_news = News()
