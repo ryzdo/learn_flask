f15f2afb07463b7abf9514dc7b3b3261ade7d648
Denis Ryzhankov <denryzhankov@gmail.com>
2023-03-12 17:47:01 +0300

    fix index

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_login import LoginManager
 
 from webapp.admin.views import blueprint as admin_blueprint
 from webapp.model import db
+from webapp.news.model import News
 from webapp.news.views import blueprint as news_blueprint
 from webapp.user.model import User
 from webapp.user.views import blueprint as user_blueprint
@@ -14,13 +15,19 @@ def create_app():
     app.config.from_pyfile("config.py")
     db.init_app(app)
 
-    login_manager = LoginManager()
-    login_manager.init_app(app)
-    login_manager.login_view = "user.login"
+    @app.route("/")
+    def index():
+        title = "Новости Python"
+        news_list = News.query.order_by(News.published.desc()).all()
+        return render_template("index.html", page_title=title, news_list=news_list)
 
     app.register_blueprint(admin_blueprint)
     app.register_blueprint(news_blueprint)
     app.register_blueprint(user_blueprint)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = "user.login"
 
     @login_manager.user_loader
     def load_user(user_id):
--- webapp/news/views.py
+++ webapp/news/views.py
@@ -6,6 +6,7 @@ blueprint = Blueprint("news", __name__)
 
 
 @blueprint.route("/")
+# @blueprint.route("/index")
 def index():
     title = "Новости Python"
     news_list = News.query.order_by(News.published.desc()).all()
