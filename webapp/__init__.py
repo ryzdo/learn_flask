52871232e2812579ceaaad109a3d1623a8db32b7
Denis Ryzhankov <denryzhankov@gmail.com>
2023-03-12 16:25:02 +0300

    refactoring webapp

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
-from flask_login import LoginManager, current_user, login_required
+from flask import Flask
+from flask_login import LoginManager
 
-from webapp.model import News, db
+from webapp.admin.views import blueprint as admin_blueprint
+from webapp.model import db
+from webapp.news.views import blueprint as news_blueprint
 from webapp.user.model import User
 from webapp.user.views import blueprint as user_blueprint
 
@@ -12,25 +14,13 @@ def create_app():
     app.config.from_pyfile("config.py")
     db.init_app(app)
 
-    @app.route("/")
-    def index():
-        title = "Новости Python"
-        news_list = News.query.order_by(News.published.desc()).all()
-        return render_template("index.html", page_title=title, news_list=news_list)
-
-    @app.route("/admin")
-    @login_required
-    def admin_index():
-        if current_user.is_admin:
-            return "Привет админ"
-        else:
-            return "Ты не админ!"
-
     login_manager = LoginManager()
     login_manager.init_app(app)
-    login_manager.login_view = "login"
+    login_manager.login_view = "user.login"
 
     app.register_blueprint(user_blueprint)
+    app.register_blueprint(admin_blueprint)
+    app.register_blueprint(news_blueprint)
 
     @login_manager.user_loader
     def load_user(user_id):
--- webapp/model.py
+++ webapp/model.py
@@ -1,37 +1,4 @@
-from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy, model
-from werkzeug.security import check_password_hash, generate_password_hash
 
 db = SQLAlchemy()
 Base: model = db.Model
-
-
-class News(Base):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    url = db.Column(db.String, unique=True, nullable=False)
-    published = db.Column(db.DateTime, nullable=False)
-    text = db.Column(db.Text, nullable=True)
-
-    def __repr__(self):
-        return "<News {} {}>".format(self.title, self.url)
-
-
-class User(Base, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    password = db.Column(db.String(128))
-    role = db.Column(db.String(10), index=True)
-
-    def __repr__(self):
-        return "<User {}>".format(self.username)
-
-    def set_password(self, password):
-        self.password = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
-
-    @property
-    def is_admin(self):
-        return self.role == "admin"
