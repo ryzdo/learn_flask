f1a1635aaad43b224056e48f0ccbffd02a0d29d2
Denis Ryzhankov <denryzhankov@gmail.com>
2023-03-12 18:43:47 +0300

    restruct templates

--- webapp/news/views.py
+++ webapp/news/views.py
@@ -6,8 +6,8 @@ blueprint = Blueprint("news", __name__)
 
 
 @blueprint.route("/")
-# @blueprint.route("/index")
+@blueprint.route("/index")
 def index():
     title = "Новости Python"
     news_list = News.query.order_by(News.published.desc()).all()
-    return render_template("inews/index.html", page_title=title, news_list=news_list)
+    return render_template("news/index.html", page_title=title, news_list=news_list)
